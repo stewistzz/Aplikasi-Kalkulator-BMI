af41f62fed45c9f24c3119ab30a8cac16f78cdc8
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2025-09-17 14:31:59 +0700

    insialisasi awal new project dari dart

---
+++ dart_aplikasi_bmi/bin/dart_aplikasi_bmi.dart
@@ -0,0 +1,5 @@
+import 'package:dart_aplikasi_bmi/dart_aplikasi_bmi.dart' as dart_aplikasi_bmi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_aplikasi_bmi.calculate()}!');
+}
---
+++ dart_aplikasi_bmi/test/dart_aplikasi_bmi_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_aplikasi_bmi/dart_aplikasi_bmi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
